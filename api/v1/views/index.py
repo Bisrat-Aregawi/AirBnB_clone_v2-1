b439f433f62c0cdeb9dbc2ad68516f5ec9b405da
Bisrat-Aregawi <bisrat_aregawi1914@yahoo.com>
2022-02-07 15:13:29 +0300

    Update api/v1/index.py to return db table stats
    For task 4 of Team-Project: - AirBnB_clone_v3

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,6 +2,8 @@
 """Creationg route for Blueprint
 """
 from api.v1.views import app_views
+from models import storage
+from flask import jsonify
 
 
 @app_views.route('/status')
@@ -10,3 +12,21 @@ def response():
     """
     dic = {"status": "OK"}
     return dic
+
+
+@app_views.route('/stats')
+def db_stats():
+    """ retrieve and show number of each objects by type
+    """
+    dictionary = {
+        "amenities": 0,
+        "cities": 0,
+        "places": 0,
+        "reviews": 0,
+        "states": 0,
+        "users": 0,
+    }
+    for v in storage.all().values():
+        dictionary[v.__tablename__] = storage.count(v.__class__)
+
+    return (jsonify(dictionary))
