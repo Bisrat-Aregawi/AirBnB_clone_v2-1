0f17ea571f2c0b3d5d4405e4a8e139b2d07b2c78
Bryanaim <isale.bryan@gmail.com>
2022-02-08 05:34:22 +0300

    fix states

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -1,95 +1,74 @@
 #!/usr/bin/python3
-"""Module handles all RESTful API actions on state"""
+""" State APIRest
+"""
+
+from models import storage
 from models.state import State
 from api.v1.views import app_views
-from models import storage
 from flask import jsonify, abort, request
 
 
 @app_views.route('/states', methods=['GET'])
-def states_list():
-    """Return a list of states in DB.
+def list_dict():
+    """ list of an objetc in a dict form
     """
-    lst = [
-        state.to_dict() for state in storage.all(State).values()
-    ]
-    return jsonify(lst)
+    lista = []
+    dic = storage.all('State')
+    for elem in dic:
+        lista.append(dic[elem].to_dict())
+    return (jsonify(lista))
 
 
-@app_views.route('/states/<state_id>', methods=['GET'])
-def state_with_id(state_id):
-    """Return state record with id `state_id` from DB
-
-    Args:
-        state_id (str): uuid of state requred
-
-    Returns:
-        Jsonified version of state record with id `state_id`
+@app_views.route('/states/<state_id>', methods=['GET', 'DELETE'])
+def state_id(state_id):
+    """ realize the specific action depending on method
     """
-    try:
-        return jsonify(storage.get(State, state_id).to_dict())
-    except Exception:
-        abort(404)
-
-
-@app_views.route('/states/<state_id>', methods=['DELETE'])
-def del_state_with_id(state_id):
-    """Delete state record with id `state_id` from DB.
-
-    Args:
-        state_id (str): uuid of state required
-
-    Returns:
-        Empty jsonified dictionary upon success
-    """
-    try:
-        marked_obj = storage.get(State, state_id)
-        storage.delete(marked_obj)
-        storage.save()
-        return jsonify({})
-    except Exception:
-        abort(404)
+    lista = []
+    dic = storage.all('State')
+    for elem in dic:
+        var = dic[elem].to_dict()
+        if var["id"] == state_id:
+            if request.method == 'GET':
+                return (jsonify(var))
+            elif request.method == 'DELETE':
+                aux = {}
+                dic[elem].delete()
+                storage.save()
+                return (jsonify(aux))
+    abort(404)
 
 
 @app_views.route('/states', methods=['POST'])
-def add_state():
-    """Insert a new state to DB
-
-    Args:
-        None
-
-    Returns:
-        Jsonified new_state created
+def add_item():
+    """ add a new item
     """
-    if not request.get_json(silent=True):
-        return (jsonify(error="Not a JSON"), 400)
-    if request.get_json().get('name'):
-        new_state = State(**request.get_json())
-        new_state.save()
-        return (jsonify(new_state.to_dict()), 201)
-    return (jsonify(error="Missing name"), 400)
+    if not request.json:
+        return jsonify("Not a JSON"), 400
+    else:
+        content = request.get_json()
+        if "name" not in content.keys():
+            return jsonify("Missing name"), 400
+        else:
+            new_state = State(**content)
+            new_state.save()
+            return (jsonify(new_state.to_dict()), 201)
 
 
 @app_views.route('/states/<state_id>', methods=['PUT'])
-def update_state(state_id):
-    """Update state with id `state_id` in DB
-
-    Args:
-        state_id (str): uuid of state requred
-
-    Returns:
-        Jsonified state record and 200 status code
-        If request is not valid JSON 400 with error message 'NOT a JSON'
+def update_item(state_id):
+    """ update item
     """
-    if not request.get_json(silent=True):
-        abort(400)
-    update_state = storage.get(State, state_id)
-    if update_state:
-        dictionary = request.get_json()
-        for k, v in dictionary.items():
-            if k != 'created_at' and k != 'updated_at' and k != 'id':
-                setattr(update_state, k, v)
-                storage.save()
-                return (jsonify(update_state.to_dict()), 200)
-    else:
-        abort(404)
+    dic = storage.all("State")
+    for key in dic:
+        if dic[key].id == state_id:
+            if not request.json:
+                return jsonify("Not a JSON"), 400
+            else:
+                forbidden = ["id", "update_at", "created_at"]
+                content = request.get_json()
+                for k in content:
+                    if k not in forbidden:
+                        setattr(dic[key], k, content[k])
+                dic[key].save()
+                return(jsonify(dic[key].to_dict()))
+    abort(404)
