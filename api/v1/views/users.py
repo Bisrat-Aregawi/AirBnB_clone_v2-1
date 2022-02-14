4c836524c7d40851b16fbc8f88bf2ceb330b64f7
Bisrat-Aregawi <bisrat_aregawi1914@yahoo.com>
2022-02-14 22:17:31 +0300

    refactor `users.py` and pep8 fixes

--- api/v1/views/users.py
+++ api/v1/views/users.py
@@ -12,29 +12,25 @@ from flask import jsonify, abort, request
 def user_list():
     """ list of an objetc in a dict form
     """
-    lista = []
-    dic = storage.all('User')
-    for elem in dic:
-        lista.append(dic[elem].to_dict())
-    return (jsonify(lista))
+    users_list = [
+        usr.to_dict() for usr in storage.all(User).values()
+    ]
+    return (jsonify(users_list))
 
 
 @app_views.route('/users/<user_id>', methods=['GET', 'DELETE'])
 def user_id(user_id):
     """ realize the specific action depending on method
     """
-    lista = []
-    dic = storage.all('User')
-    for elem in dic:
-        var = dic[elem].to_dict()
-        if var["id"] == user_id:
-            if request.method == 'GET':
-                return (jsonify(var))
-            elif request.method == 'DELETE':
-                aux = {}
-                dic[elem].delete()
-                storage.save()
-                return (jsonify(aux))
+    user = storage.get(User, user_id)
+    if user:
+        if request.method == "GET":
+            return jsonify(user.to_dict())
+        elif request.method == "DELETE":
+            user.delete()
+            storage.save()
+            storage.close()
+            return jsonify({})
     abort(404)
 
 
@@ -42,35 +38,33 @@ def user_id(user_id):
 def user_item():
     """ add a new item
     """
-    if not request.json:
-        return jsonify("Not a JSON"), 400
-    else:
-        content = request.get_json()
-        if "email" not in content.keys():
-            return jsonify("Missing email"), 400
-        if "password" not in content.keys():
-            return jsonify("Missing password"), 400
-        else:
-            new_user = User(**content)
-            new_user.save()
-            return (jsonify(new_user.to_dict()), 201)
+    user_dict = request.get_json(silent=True)
+    if user_dict:
+        if user_dict.get('email'):
+            if user_dict.get('password'):
+                new_user = User(**user_dict)
+                new_user.save()
+                storage.close()
+                return (jsonify(new_user.to_dict()), 201)
+            return (jsonify(error="Missing password"), 400)
+        return (jsonify(error="Missing email"), 400)
+    return (jsonify(error="Not a JSON"), 400)
 
 
 @app_views.route('/users/<user_id>', methods=['PUT'])
 def update_user(user_id):
     """ update an item
     """
-    dic = storage.all("User")
-    for key in dic:
-        if dic[key].id == user_id:
-            if not request.json:
-                return jsonify("Not a JSON"), 400
-            else:
-                forbidden = ["id", "email", "update_at", "created_at"]
-                content = request.get_json()
-                for k in content:
-                    if k not in forbidden:
-                        setattr(dic[key], k, content[k])
-                dic[key].save()
-                return(jsonify(dic[key].to_dict()))
-    abort(404)
+    user_dict = request.get_json(silent=True)
+    if user_dict:
+        update_me = storage.get(User, user_id)
+        if update_me:
+            forbidden = ["id", "email", "created_at", "updated_at"]
+            for k, v in user_dict.items():
+                if k not in forbidden:
+                    setattr(update_me, k, v)
+            storage.save()
+            storage.close()
+            return jsonify(update_me.to_dict())
+        abort(404)
+    return (jsonify(error="Not a JSON"), 400)
