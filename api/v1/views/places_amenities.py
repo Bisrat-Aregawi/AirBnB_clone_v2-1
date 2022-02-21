64e9a361989a6fb56a6730f7f159c8d1b9008a30
Bisrat-Aregawi <bisrat_aregawi1914@yahoo.com>
2022-02-21 14:19:59 +0300

    Refactor `place_amenities.py`
    Refactor `am_delete` function to abort with 404 if amenity is not linked
    with any place, there is no place with the given id, or there is not
    amenity with the given id
    
    Refactor `add_am` function to use relationship variables in manipulating
    amenities and places

--- api/v1/views/places_amenities.py
+++ api/v1/views/places_amenities.py
@@ -8,6 +8,7 @@ from models.user import User
 from models.amenity import Amenity
 from models.review import Review
 from api.v1.views import app_views
+from models.place import place_amenity
 from flask import jsonify, abort, request
 from os import getenv
 
@@ -18,56 +19,49 @@ type = getenv('HBNB_TYPE_STORAGE')
 def am_list(place_id):
     """ list of an objetc in dict form
     """
-    lista = []
-    dic = storage.all('Place')
-    for elem in dic:
-        if dic[elem].id == place_id:
-            var = dic[elem].amenities
-            for i in var:
-                lista.append(i.to_dict())
-            return (jsonify(lista))
+    target_place = storage.get(Place, place_id)
+    if target_place:
+        amenities = [
+            am.to_dict() for am in target_place.amenities
+        ]
+        return jsonify(amenities)
     abort(404)
 
 
-@app_views.route('/places/<place_id>/amenities/<amenity_id>',
-                 methods=['DELETE'])
+@app_views.route(
+    '/places/<place_id>/amenities/<amenity_id>',
+    methods=['DELETE']
+)
 def am_delete(place_id, amenity_id):
     """ delete the object
     """
-    p = storage.get("Place", place_id)
-    a = storage.get("Amenity", amenity_id)
-    if not a or not p:
+    target_place = storage.get(Place, place_id)
+    if not target_place:
         abort(404)
-    for elem in p.amenities:
-        if elem.id == a.id:
-            if type == 'db':
-                p.amenities.remove(a)
-            else:
-                p.amenity_ids.remove(a)
-            p.save()
-            return jsonify({})
-    abort(404)
+    target_amenity = storage.get(Amenity, amenity_id)
+    if not target_amenity:
+        abort(404)
+    if target_amenity not in target_place.amenities:
+        abort(404)
+    target_amenity.delete()
+    storage.save()
+    storage.close()
+    return jsonify({})
 
 
 @app_views.route('/places/<place_id>/amenities/<amenity_id>', methods=['POST'])
 def add_am(place_id, amenity_id):
     """ create an amenity of a city
     """
-    lista = []
-    obj = storage.get("Place", place_id)
-    p = storage.get("Place", place_id)
-    a = storage.get("Amenity", amenity_id)
-    print(a)
-    print("+++++++++++")
-    print(p)
-    if not a or not p:
+    target_place = storage.get(Place, place_id)
+    if target_place:
+        target_amenity = storage.get(Amenity, amenity_id)
+        if target_amenity:
+            if target_amenity in target_place.amenities:
+                return jsonify(target_amenity.to_dict())
+            target_place.amenities.append(target_amenity)
+            storage.save()
+            storage.close()
+            return (jsonify(target_amenity.to_dict()), 201)
         abort(404)
-    for elem in p.amenities:
-        if elem.id == a.id:
-            return jsonify(a.to_dict())
-    if type == 'db':
-        p.amenities.append(a)
-    else:
-        p.amenity_id.append(a)
-    p.save()
-    return jsonify(a.to_dict()), 201
+    abort(404)
